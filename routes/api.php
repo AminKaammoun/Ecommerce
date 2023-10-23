76b1c86ed3f6c1fed95e7108ead176335b0d6825
KMOON <93909632+AminKaammoun@users.noreply.github.com>
2023-10-23 14:45:18 +0100

    version 2

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\CategorieController;
+use App\http\Controllers\ScategorieController;
+use App\http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('categories',CategorieController::class);
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('scategories',ScategorieController::class);
+});
+
+Route::get('/scat/{idcat}',[ScategorieController::class,'showSCategorieByCAT']);
+
+Route::middleware('api')->group(function () {
+    Route::resource('articles', ArticleController::class);
+    });
